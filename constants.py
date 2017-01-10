3b1379a8d39d5719d982a0e1187cc8b3ccb5f1f8
Dan-Cristian Andronic <Cristian@nat-tvwna-inside-visitornet101-d-13446.princeton.org>
2017-01-10 01:45:04 -0500

    Moved model to separate file and added tensorboard logs

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Constants pertaining to the dataset
+
+# The images are always 96x96 pixels.
+IMAGE_SIZE = 96
+IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
+
+# output layer size is 30 (= x and y coordinates for 15 keypoints)
+OUTPUT = 30
--- model.py
+++ model.py
@@ -1,11 +1,44 @@
+import math
+
 import tensorflow as tf
 
-# Model
-x = tf.placeholder(tf.float32, [None, 96 * 96])
-W = tf.Variable(tf.zeros([96 * 96, 30]))
-b = tf.Variable(tf.zeros([30]))
-y = tf.matmul(x, W) + b
-y_ = tf.placeholder(tf.float32, [None, 30])
-mse = tf.reduce_mean(tf.square(y - y_))
+from constants import IMAGE_PIXELS, OUTPUT
 
+def inference(images, hidden1_units, hidden2_units):
+	with tf.name_scope('layer1'):
+		# Placeholder Model
+		with tf.name_scope('weights'):
+			W = tf.Variable(tf.zeros([96 * 96, 30]))
+		with tf.name_scope('biases'):
+			b = tf.Variable(tf.zeros([30]))
+		with tf.name_scope('Wx_plus_b'):
+			y = tf.matmul(images, W) + b
+		return y
+
+def loss(y, y_):
+	# y - prediction
+	# y_ - ground truth
+	with tf.name_scope('loss'):
+		return tf.reduce_mean(tf.square(y - y_))
+
+
+def training(loss, learning_rate):
+	with tf.name_scope('train'):
+		# Create the gradient descent optimizer with the given learning rate.
+		optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+		# Create a variable to track the global step.
+		global_step = tf.Variable(0, name='global_step', trainable=False)
+		# Use the optimizer to apply the gradients that minimize the loss
+		# (and also increment the global step counter) as a single training step.
+		train_op = optimizer.minimize(loss, global_step=global_step)
+		return train_op
+
+
+# Best guess for leaderboard score
+def evaluation(y, y_):
+	# y - prediction
+	# y_ - ground truth
+	with tf.name_scope('eval'):
+		mse = tf.reduce_mean(tf.square(y - y_))
+		return mse#math.sqrt(mse) * 48
 
